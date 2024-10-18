b699dcaf8eb69b50f182f881d4cd2a18ca9572a4
chikastm17 <119578505+chikastm17@users.noreply.github.com>
2024-10-18 16:58:54 +0700

    Fitur list

---
+++ resources/views/barang ditemukan.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Barang Ditemukan</title>
+    <!-- Menghubungkan CSS -->
+    <link rel="stylesheet" href="styles.css"> <!-- Pastikan untuk menambahkan rel -->
+</head>
+<body>
+    <div class="container mt-5">
+        <h1 class="text-center">Data Barang Ditemukan</h1>
+        <table class="table table-bordered table-striped">
+            <thead class="table-dark">
+                <tr>
+                    <th>Nama Pelapor</th>
+                    <th>Nomor Telepon</th>
+                    <th>Nama Barang yang di Temukan</th>
+                    <th>Lokasi Penemuan</th>
+                    <th>Deskripsi Barang</th>
+                    <th>Tanggal Penemuan</th>
+                    <th>Kategori Barang</th>
+                    <th>Gambar Barang</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Sefania</td>
+                    <td>081234567572</td>
+                    <td>Tumbler</td>
+                    <td>Kantin 2 lantai 1</td>
+                    <td>Tupperware Ungu tutup Biru</td>
+                    <td>2024-10-15</td>
+                    <td>Lainnya</td>
+                    <td><img src="image/tumbler.jpeg" alt="Dompet" class="img-thumbnail" style="width: 60px;"></td>
+                </tr>
+                <tr>
+                    <td>Kezia</td>
+                    <td>08234567123</td>
+                    <td>Payung</td>
+                    <td>GD 714</td>
+                    <td>Warna hitam anti uv</td>
+                    <td>2024-10-16</td>
+                    <td>Lainnya</td>
+                    <td><img src="image/payung.jpeg" alt="Handphone" class="img-thumbnail" style="width: 60px;"></td>
+                </tr>
+                <tr>
+                    <td>Wilson</td>
+                    <td>083456789234</td>
+                    <td>TWS</td>
+                    <td>Saung GD 5</td>
+                    <td>TWS thinkplus warna hitam</td>
+                    <td>2024-10-17</td>
+                    <td>Elektronik</td>
+                    <td><img src="image/tws.jpeg" alt="Ransel" class="img-thumbnail" style="width: 60px;"></td>
+                </tr>
+               
+                <!-- Tambahkan lebih banyak baris di sini -->
+            </tbody>
+        </table>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
